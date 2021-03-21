b46fa156fe6a7f57da020974adaafebc25b8d01b
Kyle Kizirian <kyle.kizirian@gmail.com>
2021-03-20 21:12:35 -0400

    fix unnecessary else statement

--- src/honey/merge.py
+++ src/honey/merge.py
@@ -23,8 +23,8 @@ def merge(list1: List, list2: List) -> List:
 
     if list1[0] < list2[0]:
         return [list1[0]] + merge(list1[1:], list2)
-    else:
-        return [list2[0]] + merge(list1, list2[1:])
+
+    return [list2[0]] + merge(list1, list2[1:])
 
 
 assert merge([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
